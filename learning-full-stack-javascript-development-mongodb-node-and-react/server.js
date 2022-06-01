4d3b1be51ea76ec897b42e2eb37e7887e0aaaad5
System Administrator <root@Pavels-MacBook-Pro.local>
2022-06-02 02:13:18 +0300

    The HTTP/HTTPS modules

--- learning-full-stack-javascript-development-mongodb-node-and-react/server.js
+++ learning-full-stack-javascript-development-mongodb-node-and-react/server.js
@@ -1,7 +1,33 @@
-import config, { nodeEnv, logStars} from './config';
+import http from 'http';
+
+const server = http.createServer();
+
+server.listen(8080);
+
+server.on('request', (req, res) => {
+  res.write('Hello HTTP!\n');
+  setTimeout(() => {
+    res.write('I can stream!\n');
+    res.end();
+  }, 3000);
+});
+
+/*import https from 'https';
+
+https.get('https://www.google.com/', res => {
+  console.log('Response status code: ', res.statusCode);
+
+  res.on('data', chunk => {
+    console.log(chunk.toString());
+  });
+});
+*/
+
+/*import config, { nodeEnv, logStars} from './config';
 
 // import './config';
 
 console.log(config, nodeEnv);
 
 logStars('Function');
+*/
