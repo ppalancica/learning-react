151870aa3e87aa9a61f05adf2a93342db5807d77
System Administrator <root@Pavels-MacBook-Pro.local>
2022-06-02 02:39:24 +0300

    Playing with the Express server

--- learning-full-stack-javascript-development-mongodb-node-and-react/server.js
+++ learning-full-stack-javascript-development-mongodb-node-and-react/server.js
@@ -1,4 +1,27 @@
-import http from 'http';
+import express from 'express';
+import config from './config';
+// import fs from 'fs';
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+server.use(express.static('public'));
+/*server.get('/about.html', (req, res) => {
+  // res.send('The about page');
+  fs.readFile('./about.html', (err, data) => {
+    res.send(data.toString());
+  });
+});
+*/
+
+server.listen(config.port, () => {
+  console.info('Express listening on port: ', config.port);
+});
+
+/*import http from 'http';
 
 const server = http.createServer();
 
@@ -11,6 +34,7 @@ server.on('request', (req, res) => {
     res.end();
   }, 3000);
 });
+*/
 
 /*import https from 'https';
 
