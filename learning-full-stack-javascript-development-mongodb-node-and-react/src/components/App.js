8e988cc146089448945a570fa2f23acfa9359f2c
System Administrator <root@Pavels-MacBook-Pro.local>
2022-06-02 16:25:32 +0300

    Fetching data from a remote api

--- learning-full-stack-javascript-development-mongodb-node-and-react/src/components/App.js
+++ learning-full-stack-javascript-development-mongodb-node-and-react/src/components/App.js
@@ -2,31 +2,30 @@ import React from 'react';
 import Header from './Header';
 import data from '../testData.json';
 import ContestPreview from './ContestPreview';
+import axios from 'axios';
 
-// If we need to use state or lif-cycle of the component
 class App extends React.Component {
   state = {
     pageHeader: "Naming Contests",
     contests: []
   };
-  // state = { test: 42 };
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { test: 42 };
-  // }
 
   componentDidMount() {
-    // Ajax calls, timers, listeners to other events etc.
-    console.log('componentDidMount()...');
-    //console.log(data);
-    this.setState({
-      contests: data.contests
-    });
+    axios.get('/api/contests')
+      .then(resp => {
+        console.log(resp.data.contests);
+        this.setState({
+          contests: data.contests
+        });
+      })
+      .catch(console.error);
+
+    // this.setState({
+    //   contests: data.contests
+    // });
   }
 
   componentWillUnmount() {
-    // clean timers, listeners
-    console.log('componentWillUnmount()...');
   }
 
   render() {
@@ -41,48 +40,6 @@ class App extends React.Component {
       </div>
     );
   };
-
-  /*render() {
-    return (
-      <div className="App">
-        <Header message={this.state.pageHeader} />
-        <div>
-          {this.props.contests.map(contest =>
-            <ContestPreview key={contest.id} {...contest} />
-          )}
-        </div>
-      </div>
-    );
-  };*/
-
-  //<ContestPreview {...this.props.contests[0]} />
-
-  /*render() {
-    return (
-      <div className="App">
-        <Header message={this.state.pageHeader} />
-        <div>
-          {this.state.test}
-        </div>
-      </div>
-    );
-  }*/
 }
-// We can change the component's state from the browser's console:
-// $r.setState({pageHeader: "Some text"})
 
-// React.createClass
-// extends React.Component
-
-/*const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests" />
-      <div>
-        ...
-      </div>
-    </div>
-  );
-};
-*/
 export default App;
---
+++ learning-full-stack-javascript-development-mongodb-node-and-react/src/components/App_v1.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import Header from './Header';
+import data from '../testData.json';
+import ContestPreview from './ContestPreview';
+
+// If we need to use state or lif-cycle of the component
+class App_v1 extends React.Component {
+  state = {
+    pageHeader: "Naming Contests",
+    contests: []
+  };
+  // state = { test: 42 };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { test: 42 };
+  // }
+
+  componentDidMount() {
+    // Ajax calls, timers, listeners to other events etc.
+    console.log('componentDidMount()...');
+    //console.log(data);
+    this.setState({
+      contests: data.contests
+    });
+  }
+
+  componentWillUnmount() {
+    // clean timers, listeners
+    console.log('componentWillUnmount()...');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          {this.state.contests.map(contest =>
+            <ContestPreview key={contest.id} {...contest} />
+          )}
+        </div>
+      </div>
+    );
+  };
+
+  /*render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          {this.props.contests.map(contest =>
+            <ContestPreview key={contest.id} {...contest} />
+          )}
+        </div>
+      </div>
+    );
+  };*/
+
+  //<ContestPreview {...this.props.contests[0]} />
+
+  /*render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          {this.state.test}
+        </div>
+      </div>
+    );
+  }*/
+}
+// We can change the component's state from the browser's console:
+// $r.setState({pageHeader: "Some text"})
+
+// React.createClass
+// extends React.Component
+
+/*const App = () => {
+  return (
+    <div className="App">
+      <Header message="Naming Contests" />
+      <div>
+        ...
+      </div>
+    </div>
+  );
+};
+*/
+export default App_v1;
