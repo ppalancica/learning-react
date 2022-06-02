f88e7bbc2e406a9e1646b089ddbf7ca85b49f879
System Administrator <root@Pavels-MacBook-Pro.local>
2022-06-02 13:41:53 +0300

    Components state

--- learning-full-stack-javascript-development-mongodb-node-and-react/src/components/App.js
+++ learning-full-stack-javascript-development-mongodb-node-and-react/src/components/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import Header from './Header';
 
-const App = () => {
+// If we need to use state or lif-cycle of the component
+class App extends React.Component {
+  state = {
+    pageHeader: "Naming Contests"
+  };
+  // state = { test: 42 };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { test: 42 };
+  // }
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          {this.state.test}
+        </div>
+      </div>
+    );
+  }
+}
+// We can change the component's state from the browser's console:
+// $r.setState({pageHeader: "Some text"})
+
+// React.createClass
+// extends React.Component
+
+/*const App = () => {
   return (
     <div className="App">
       <Header message="Naming Contests" />
@@ -11,5 +38,5 @@ const App = () => {
     </div>
   );
 };
-
+*/
 export default App;
